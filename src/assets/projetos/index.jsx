c54f9433434943e087da1e071216ee27fcc04049
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-02-17 23:19:20 -0300

    .

--- src/assets/projetos/index.jsx
+++ src/assets/projetos/index.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-import projetosData from './projetos.json'; 
-
-import imagens from `./imagens/imagem${i}`
+import projetosData from './projetos.json';
+import imagem1 from '../../../public/imagens/imagem1.png'; // Importe a imagem aqui
 
 // Estilizando a galeria
 const Gallery = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    gap: 20px;
-    margin: 20px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
 `;
 
 // Estilizando o cartão do projeto
 const ProjectCard = styled.div`
-    background-color: #f9f9f9;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #f9f9f9;
+  padding: 15px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 45%;
+  height: 30%;
+
+  img {
+    width: 100%;
+    height: 60%;
+  }
 `;
 
 const Projetos = () => {
-    return (
-        <Gallery>
-            {projetosData.projetos.map(projeto => (
-                <ProjectCard key={projeto.id}>
-                    <img src={require(`./imagens/imagem${projeto.id}.png`).default}  alt={projeto.titulo} />
-                    <div>
-                        <h3>{projeto.titulo}</h3>
-                        <p>{projeto.descricao}</p>
-                        <a href={projeto.link} target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-                    </div>
-                </ProjectCard>
-            ))}
-        </Gallery>
-    );
-}
+  return (
+    <Gallery>
+      {projetosData.projetos.map((projeto) => (
+        <ProjectCard key={projeto.id}>
+          <img src={projeto.imagem} alt={projeto.titulo} />
+          <div>
+            <h3>{projeto.titulo}</h3>
+            <p>{projeto.descricao}</p>
+            <a href={projeto.link} target="_blank" rel="noopener noreferrer">
+              Ver Projeto
+            </a>
+          </div>
+        </ProjectCard>
+      ))}
+    </Gallery>
+  );
+};
 
 export default Projetos;
